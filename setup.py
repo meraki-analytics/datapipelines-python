5f1cda519202f8f4d2979ec16bd6416c78ae1b76
Rob Rua <robertrua@gmail.com>
2017-04-21 20:58:57 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+from os import path
+
+from setuptools import setup, find_packages
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, "README.rst"), encoding="UTF-8") as readme:
+    long_description = readme.read()
+
+setup(
+    name="datapipelines",
+    version="0.0.1",
+    author="Rob Rua",
+    author_email="robrua@alumni.cmu.edu",
+    url="https://github.com/meraki-analytics/datapipelines",
+    description="Caching abstraction layer for orchestrating multiple cache tiers",
+    long_description=long_description,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Database :: Front-Ends"
+    ],
+    license="MIT",
+    packages=find_packages(),
+    zip_safe=True,
+    install_requires=[
+        "networkx"
+    ]
+)
