b4d5b2896d598efca8ab2aa078e6c21ae9fa8177
Rob Rua <robertrua@gmail.com>
2017-04-29 23:04:12 -0400

    export Composite source/sinks/transformer

--- datapipelines/__init__.py
+++ datapipelines/__init__.py
@@ -1,8 +1,8 @@
 from .common import PipelineContext, UnsupportedError, NotFoundError, TYPE_WILDCARD
 from .pipelines import DataPipeline, NoConversionError
 from .queries import Query, QueryValidationError, QueryValidatorStructureError
-from .sinks import DataSink
-from .sources import DataSource
-from .transformers import DataTransformer
+from .sinks import DataSink, CompositeDataSink
+from .sources import DataSource, CompositeDataSource
+from .transformers import DataTransformer, CompositeDataTransformer
 
-__all__ = ["DataTransformer", "DataPipeline", "NoConversionError", "Query", "QueryValidationError", "QueryValidatorStructureError", "DataSource", "DataSink", "PipelineContext", "UnsupportedError", "NotFoundError", "TYPE_WILDCARD"]
+__all__ = ["DataTransformer", "CompositeDataTransformer", "DataPipeline", "NoConversionError", "Query", "QueryValidationError", "QueryValidatorStructureError", "DataSource", "CompositeDataSource", "DataSink", "CompositeDataSink", "PipelineContext", "UnsupportedError", "NotFoundError", "TYPE_WILDCARD"]
